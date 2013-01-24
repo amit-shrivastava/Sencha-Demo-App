b4ec8a9e3410af5743a9d9a7eab1fd10ef1d845a
Joker-SMF <makerzzz.zzzz@gmail.com>
2013-01-24 21:15:38 +0530

    Search implemented

--- app/view/mainView.js
+++ app/view/mainView.js
@@ -1,17 +1,22 @@
 Ext.define('SenchaNote.view.mainView',{
-    //extend: 'Ext.Container',
     extend: 'Ext.List', 
     xtype: 'mainview',
     requires: [
         'Ext.dataview.List',
+        'Ext.field.Search',
+        'Ext.Toolbar',
     ],
     alias: "widget.mainView",
-    
+
     config: {
         layout: {
             type: 'fit'
         },
+        xtype: 'list',
+        itemId:"booksList",
         itemTpl: '<div>{volumeInfo.title}</div>',
+        onItemDisclosure: true,
+        emptyText: 'test',
         store: "mainViewStore",
         items: [{
             xtype: "toolbar",
@@ -22,21 +27,31 @@ Ext.define('SenchaNote.view.mainView',{
             docked:'bottom',
             items:[{
                 xtype: 'searchfield',
+                width: '90%',
                 itemId:'books_search',
                 id:'books_search',
                 placeHolder: 'Search Books'  
             }]
-        }
-        ],
+        }],
         listeners: [{
             delegate: "#booksList",
             event: "disclose",
             fn: "onBooksListDisclose"
+        }, {
+            delegate: "#books_search",
+            event: "keyup",
+            fn: "onSearchTextInput"
         }],
     },
 
     onBooksListDisclose: function (list, record, target, index, evt, options) {
+        console.log('clicked');
         var id = record.data.id;
         this.fireEvent('getBookDetails', this, id);
+    },
+
+    onSearchTextInput : function(field) {
+        var value = field.getValue();
+        this.fireEvent('searchInputReceived', this, value);
     }
 });
