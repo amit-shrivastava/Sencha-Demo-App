71deac82cf546170d1d1602acd3869054c4f6410
Joker-SMF <makerzzz.zzzz@gmail.com>
2013-01-24 23:41:17 +0530

    Trying to understand model relationships

--- app/model/mainViewModel.js
+++ app/model/mainViewModel.js
@@ -1,6 +1,49 @@
 Ext.define('SenchaNote.model.mainViewModel', {
     extend: 'Ext.data.Model',
     config : {
-        fields: ['id', 'volumeInfo'],   
+        //fields: ['id', 'volumeInfo'],
+        fields: [
+           // {name: 'volumeInfo', type: 'string'},
+            {name: 'volumeInfo', convert: 
+            function(value, record) {
+                //console.log(value)
+
+                console.log(value.title)
+                return value.title;
+            }
+        }
+        ],
     }
 });
+
+
+/*Ext.define('SenchaNote.model.mainViewModel', {
+    extend: 'Ext.data.Model',
+    config : {
+        fields: [
+            {name: 'volumeInfo', type: 'string'},
+        ],
+        hasOne: {
+            associationKey: 'volumeInfo',
+            model: 'SenchaNote.model.titleModel'
+        },
+    }
+});
+
+
+Ext.define("SenchaNote.model.titleModel", {
+    extend: 'Ext.data.Model',
+    uses: [
+        'SenchaNote.model.mainViewModel'
+    ],
+    config: {
+        fields: [
+            {
+                name: 'title'
+            }
+        ],
+        belongsTo: {
+            model: 'MyApp.model.mainViewModel'
+        }
+    }
+});*/
